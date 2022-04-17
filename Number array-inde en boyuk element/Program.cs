ccb61292ee678606e54a6a1fc9592dffa69a52c9
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-17 16:19:03 +0400

    classwork 15.04.22

---
+++ Number array-inde en boyuk element/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Number_array_inde_en_boyuk_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int num1 = Convert.ToInt32(Console.ReadLine());
+          int num2 = Convert.ToInt32(Console.ReadLine());
+          int num3 = Convert.ToInt32(Console.ReadLine());
+          int num4 = Convert.ToInt32(Console.ReadLine());
+            
+          
+            
+          int[] numbers = { num1, num2, num3, num4 };
+            int max = numbers[0];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (max < numbers[i])
+                {
+                    max = numbers[i];
+                    Console.WriteLine(numbers[i]);
+                }
+            
+            }
+        } 
+        
+    }  
+    {
+       
+    }
+}
